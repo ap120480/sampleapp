d542c40a87b7d5b792b4045d5e5306a691266ffe
ap120480 <100367151+ap120480@users.noreply.github.com>
2022-02-27 02:53:36 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a> Testing 123456
+          Welcome to <a href="https://nextjs.org">Next.js!</a> Testing ABCDEF
         </h1>
 
         <p className={styles.description}>
